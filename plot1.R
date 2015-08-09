39300c616b3d4d4576f8a2d50d0eac161ca78335
domlloyd <domlloyd@gmail.com>
2015-08-09 19:04:11 +0100

    Final plot 1

--- plot1.R
+++ plot1.R
@@ -13,39 +13,38 @@
 
 
 # This script uses:
-# - the "dplyr" library for selecting from the data set
 # - the "readr" library for fast reading of files using read_table
-# - the "ggplot2" library for plotting graphs
-# library("dplyr")
 library("readr")
-# library("ggplot2")
 
 # the raw data has been unzipped to our RProject directory
-# We sill setwd to the project directory we have forked. 
-# NB our working directory sits one level up from our R working directory
-setwd("..//ExData_Plotting1")
-# If running this program make sure the raw data file
+# We setwd to the project directory we have forked. 
+# NB If running this program make sure the raw data file
 # household_power_consumption.txt hs been unzipped and put in to the
-# working directory shown above.
+# working directory shown below.
 
-# clear the workspace
+setwd("..//ExData_Plotting1")
+
+
+# clear the workspace - I do this for ease of seeing which variables have been generated. 
 rm(list=ls())
 
-
+# read the household power consumption data
 consumption_obs_all <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 
 # convert Date field from chr to POSIXlt
 consumption_obs_all$Date <- strptime(consumption_obs_all$Date,'%d/%m/%Y')
 
-# select only the dates we need
+# select only the dates we need - 2007-02-01 and 2007-02-02
 consumption_obs_req <- consumption_obs_all[which(consumption_obs_all$Date == '2007-02-01' 
                                                  | consumption_obs_all$Date == '2007-02-02'), ]  
 
-# open the png file
+# open the png file for writing - this is where the plot will be stored
 png(filename = 'plot1.png', width = 480, height = 480)
-# plot the histogram
+
+# plot the histogram as required by the assignment
 hist(as.numeric(consumption_obs_req$Global_active_power), col='red', main='Global Active Power',
         xlab = 'Global Active Power (kilowatts)')
-# close the device
+
+# close the png file after writing
 dev.off()
  
